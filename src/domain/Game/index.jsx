cdc528cf7ee0f25af6c44a8e27933cc86933ef43
Nguyen Tuan Dat <53074785+itdat@users.noreply.github.com>
2020-10-20 03:54:35 +0700

    Refactor Game component from class to functional and use useState

--- src/domain/Game/index.jsx
+++ src/domain/Game/index.jsx
@@ -1,32 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../../components/Navbar";
 import Notification from "./Notification";
 import Board from "./Board";
 import History from "./History";
 import { calculateWinner } from "./services";
 
-class Game extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      size: 3,
-      history: [
-        {
-          squares: Array(9).fill(null),
-          winner: null,
-          winMoves: [],
-          move: null,
-        },
-      ],
-      stepNumber: 0,
-      xIsNext: true,
-      winSteps: 3,
-      isAsc: false,
-    };
-  }
+const Game = () => {
+  const [gameSize, setGameSize] = useState(3);
+  const [history, setHistory] = useState([
+    {
+      squares: Array(9).fill(null),
+      winner: null,
+      winMoves: [],
+      move: null,
+    },
+  ]);
+  const [stepNumber, setStepNumber] = useState(0);
+  const [xIsNext, setPlayer] = useState(true);
+  const [winSteps, setWinSteps] = useState(3);
+  const [isAsc, setSort] = useState(false);
 
-  handleCellClick = (i) => {
-    const { size, winSteps, history, stepNumber, xIsNext } = this.state;
+  const handleCellClick = (i) => {
     const current = history.slice(0, stepNumber + 1).reverse()[0];
     const player = xIsNext ? "x" : "o";
 
@@ -35,95 +29,83 @@ class Game extends React.Component {
       return;
     }
     newSquares[i] = player;
-    const move = { x: i % size, y: Math.floor(i / size) };
+    const move = { x: i % gameSize, y: Math.floor(i / gameSize) };
     const winMoves = calculateWinner(newSquares, winSteps, i, player);
     const winner = winMoves.length !== 0 ? player : null;
 
     const droppedHistory = history.slice(0, stepNumber + 1);
-    this.setState({
-      history: [
-        ...droppedHistory,
-        {
-          squares: newSquares,
-          winner,
-          winMoves,
-          move,
-        },
-      ],
-      stepNumber: droppedHistory.length,
-      xIsNext: !xIsNext,
-    });
+
+    setHistory([
+      ...droppedHistory,
+      {
+        squares: newSquares,
+        winner,
+        winMoves,
+        move,
+      },
+    ]);
+    setStepNumber(droppedHistory.length);
+    setPlayer(!xIsNext);
   };
 
-  jumpTo = (step) => {
-    const { stepNumber } = this.state;
+  const jumpTo = (step) => {
     if (step !== stepNumber) {
-      this.setState({
-        stepNumber: step,
-        xIsNext: step % 2 === 0,
-      });
+      setStepNumber(step);
+      setPlayer(step % 2 === 0);
     }
   };
 
-  reverseSort = () => {
-    this.setState({ ...this.state, isAsc: !this.state.isAsc });
+  const reverseSort = () => {
+    setSort(!isAsc);
   };
 
-  handleChangeGameSize = (e) => {
+  const handleChangeGameSize = (e) => {
     const newSize = parseInt(e.target.value);
-    this.setState({
-      ...this.state,
-      size: newSize,
-      history: [
-        {
-          squares: Array(newSize * newSize).fill(null),
-          winner: null,
-          winMoves: [],
-          move: null,
-        },
-      ],
-      stepNumber: 0,
-      xIsNext: true,
-      winSteps: newSize < 5 ? newSize : 5,
-      isAsc: false,
-    });
+    setGameSize(newSize);
+    setHistory([
+      {
+        squares: Array(newSize * newSize).fill(null),
+        winner: null,
+        winMoves: [],
+        move: null,
+      },
+    ]);
+    setStepNumber(0);
+    setPlayer(true);
+    setWinSteps(newSize < 5 ? newSize : 5);
+    setSort(false);
   };
 
-  render() {
-    const { size, history, isAsc, stepNumber, xIsNext } = this.state;
-    const current = { ...history[stepNumber] };
-    const squares = [...current.squares];
-    const checkedCells = squares.reduce((count, square) => count + (square !== null ? 1 : 0), 0);
-
-    const status = current.winner ? "Winner: " : checkedCells < size * size ? "Next player: " : "No winner!";
-    const player = current.winner ? current.winner : checkedCells < size * size ? (xIsNext ? "x" : "o") : "";
-    return (
-      <React.Fragment>
-        <Navbar onChangeGameSize={this.handleChangeGameSize} />
-
-        <section id="main-content">
-          <div className="text-center">
-            <Notification
-              status={status}
-              player={
-                player !== "" && <img className="align-middle" height="30px" src={`./${player}.svg`} alt={player} />
-              }
+  const current = { ...history[stepNumber] };
+  const squares = [...current.squares];
+  const checkedCells = squares.reduce((count, square) => count + (square !== null ? 1 : 0), 0);
+  const status = current.winner ? "Winner: " : checkedCells < gameSize * gameSize ? "Next player: " : "No winner!";
+  const player = current.winner ? current.winner : checkedCells < gameSize * gameSize ? (xIsNext ? "x" : "o") : "";
+  return (
+    <React.Fragment>
+      <Navbar onChangeGameSize={handleChangeGameSize} />
+      <section id="main-content">
+        <div className="text-center">
+          <Notification
+            status={status}
+            player={
+              player !== "" && <img className="align-middle" height="30px" src={`./${player}.svg`} alt={player} />
+            }
+          />
+          <div id="game-wrapper">
+            <Board winMoves={current.winMoves} squares={squares} onClick={handleCellClick} />
+            <History
+              isAsc={isAsc}
+              stepNumber={stepNumber}
+              history={history}
+              switchSortOption={reverseSort}
+              jumpTo={jumpTo}
             />
-            <div id="game-wrapper">
-              <Board winMoves={current.winMoves} squares={squares} onClick={this.handleCellClick} />
-              <History
-                isAsc={isAsc}
-                stepNumber={stepNumber}
-                history={history}
-                switchSortOption={this.reverseSort}
-                jumpTo={this.jumpTo}
-              />
-            </div>
           </div>
-        </section>
-      </React.Fragment>
-    );
-  }
-}
+        </div>
+      </section>
+    </React.Fragment>
+  );
+};
 
 export default Game;
